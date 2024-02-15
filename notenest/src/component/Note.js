5c526313793132f0dff8b4db32fa388ecb40b126
Ganne Supriya <gannesupriya@gmail.com>
2024-02-15 11:37:49 +0530

    committed all files

---
+++ notenest/src/component/Note.js
@@ -0,0 +1,10 @@
+import React from "react";
+function Note(){
+    return(
+        <div className="note">
+            <h1>First Note</h1>
+            <p>Java </p>
+        </div>
+    );
+}
+export default Note;
