f2f9677670f955b773297dacc6735c0e0632cf03
Ganne Supriya <gannesupriya@gmail.com>
2024-02-15 15:31:17 +0530

    second commit

--- notenest/src/component/App.js
+++ notenest/src/component/App.js
@@ -2,11 +2,23 @@ import React from "react";
 import Header from "./Header.js"
 import Footer from "./Footer.js"
 import Note from "./Note.js";
+import notes from "../notes.js";
 function App(){
   return (
   <div>
     <Header/>
-    <Note/>
+   
+    {notes.map(noteItem =>(
+      <Note
+      key={noteItem.id}
+      title={noteItem.title}
+      content={noteItem.content}
+
+      />
+
+    ))}
+    
+  
     <Footer/>
   </div>
   );
