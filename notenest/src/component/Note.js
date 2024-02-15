d209c38019edd0e1e24117b488c4799c7f280c3d
Ganne Supriya <gannesupriya@gmail.com>
2024-02-15 15:31:37 +0530

    second commit

--- notenest/src/component/Note.js
+++ notenest/src/component/Note.js
@@ -1,9 +1,9 @@
 import React from "react";
-function Note(){
+function Note(props){
     return(
         <div className="note">
-            <h1>First Note</h1>
-            <p>Java </p>
+            <h1>{props.title}</h1>
+            <p>{props.content} </p>
         </div>
     );
 }
