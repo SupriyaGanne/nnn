d1ef2cd69cb04ed64a2595aec3ad8054fb170be6
Ganne Supriya <gannesupriya@gmail.com>
2024-03-10 12:39:13 +0530

    #commited

--- notenest/src/component/App.js
+++ notenest/src/component/App.js
@@ -1,26 +1,44 @@
-import React from "react";
+import React ,{useState} from "react";
 import Header from "./Header.js"
 import Footer from "./Footer.js"
 import Note from "./Note.js";
 import notes from "../notes.js";
+import CreateArea from "./CreateArea.js";
 function App(){
+
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
   return (
-  <div>
-    <Header/>
-   
-    {notes.map(noteItem =>
-      <Note
-      key={noteItem.id}
-      title={noteItem.title}
-      content={noteItem.content}
-
-      />
-
-    )}
-    
-  
-    <Footer/>
-  </div>
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
   );
 }
 export default App;
